a498c189fa01c5db041f6d5488d756d0373a4c0c
amirkhasru123 <amirkhasru3e@gmail.com>
2025-06-09 01:08:00 +0600

    nets are cool

--- verilog/chipverify/2datatypes/net.v
+++ verilog/chipverify/2datatypes/net.v
@@ -1,50 +1,60 @@
 module net_types;
-  // wire 
-  wire net_wire;
-  assign net_wire = 1'b1;
-  assign net_wire = 1'b0; // multiple drivers possible; unknown x will be shown as output.
+    //wire 
+    wire net_wire;
+    assign net_wire = 1'b1;
+    assign net_wire = 1'b0; //multiple drivers possible; unknown x will be shown as output.
 
-  // uwire
-  uwire net_uwire;
-  assign net_uwire = 1'b1;
-  //assign net_uwire = 1'b0; // Uncommenting this will get an error. Multiple drivers not possible.
+    //uwire
+    uwire net_uwire;
+    assign net_uwire = 1'b1;
+    //assign net_uwire = 1'b0; //Uncommenting this will get an error. Multiple drivers not possible.
 
-  //tri
-  logic dev1 = 1, dev2 = 0;
-  wire [1:0] dev1_value = 2'b11;
-  wire [1:0] dev2_value = 2'b10;
-  tri [1:0] net_tri;
+    //tri
+    logic dev1 = 1, dev2 = 0;
+    wire [1:0] dev1_value = 2'b11;
+    wire [1:0] dev2_value = 2'b10;
+    tri [1:0] net_tri;
+    //Multiple drivers
+    assign net_tri = (dev1) ? dev1_value : 2'bzz;
+    assign net_tri = (dev2) ? dev2_value : 2'bzz;
 
-  //Multiple drivers:
-  assign net_tri = (dev1) ? dev1_value : 2'bzz;
-  assign net_tri = (dev2) ? dev2_value : 2'bzz;
+    //wor
+    wor net_wor;
+    assign net_wor = 1'b0;
+    assign net_wor = 1'b1;
+    assign net_wor = 1'b1; //All drivers are ored together
 
-  // wor
-  wor net_wor;
-  assign net_wor = 1'b0;
-  assign net_wor = 1'b1;
+    //wand
+    wand net_wand;
+    assign net_wand = 1'b1;
+    assign net_wand = 1'b1; //All drivers are anded together
 
-  // wand
-  wand net_wand;
-  assign net_wand = 1'b1;
-  assign net_wand = 1'b0;
+	//trior
+	trior net_trior;
+	assign net_trior = 1'b1;
+	assign net_trior = 1'b0; //tri, but conflict = ored together
 
-  // tri0
-  tri0 net_tri0;
-  assign net_tri0 = 1'bz;
-  assign net_tri0 = 1'b1;
+	//trinad
+	trior net_triand;
+	assign net_triand = 1'b1;
+	assign net_triand = 1'b0; //tri, but conflict = anded together
 
-  // tri1
-  tri1 net_tri1;
-  assign net_tri1 = 1'bz;
+    //tri0
+    tri0 net_tri0;
+    assign net_tri0 = 1'bz;
+    assign net_tri0 = 1'b1; //tri, default = 0
 
-  // supply0
-  supply0 net_supply0;
+    //tri1
+    tri1 net_tri1;
+    assign net_tri1 = 1'bz; //tri, default = 1
 
-  // supply1
-  supply1 net_supply1;
+    // supply0
+    supply0 net_supply0; //indicates ground
 
-  // iverilog doesn't support trireg
+    // supply1
+    supply1 net_supply1; //indicates voltage
+
+    // iverilog doesn't support trireg
 
   initial begin
 	#5;
@@ -67,6 +77,10 @@ module net_types;
 
     $display("net_wor      = %b", net_wor);
     $display("net_wand     = %b", net_wand);
+
+    $display("net_trior = %b", net_trior);
+    $display("net_triand = %b", net_triand);
+	  
     $display("net_tri0     = %b", net_tri0);
     $display("net_tri1     = %b", net_tri1);
     $display("net_supply0  = %b", net_supply0);
