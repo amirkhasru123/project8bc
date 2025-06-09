ff0c81b9b1c204a00e8ccf43de7ea86830f9ea9b
amirkhasru123 <amirkhasru3e@gmail.com>
2025-06-09 12:34:27 +0600

    chapter 2 revised, amir

--- verilog/chipverify/2datatypes/strength.v
+++ verilog/chipverify/2datatypes/strength.v
@@ -1,15 +1,21 @@
 module strength_example;
-  wire net_example;
+    wire net_example;
+    wire strongIsStrong;
 
-  assign (strong1, strong0) net_example = 1'b0;
-  assign (weak1, weak0) net_example = 1'b1;
+	assign (weak1,weak0) strongIsStrong = 1'b0; //weak0 will be strengthen
+	assign (strong1,strong0) strongIsStrong = 1'b1; //strong1 will be strengthen
+	//strong1 will lead
+
+    assign (strong1, strong0) net_example = 1'b0;
+    assign (weak1, weak0) net_example = 1'b1;
 	//here strong will override weak. so the value in net_examplewill remain unchanged
 
-  initial begin
+initial begin
     #5;
     $display("net_example = %b", net_example);
+	#5
+    $display("strongIsStrong = %b", strongIsStrong);
     $finish;
-  end
-
+end
 endmodule
 
